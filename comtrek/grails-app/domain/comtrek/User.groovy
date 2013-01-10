23cfdc3eee7d81c18201e33c00a9ed2fbd8992a4
Paolo <paolo.devathaire@gmail.com>
2013-01-10 22:16:59 +0100

    User domain class

---
+++ comtrek/grails-app/domain/comtrek/User.groovy
@@ -0,0 +1,24 @@
+package comtrek
+
+class User {
+
+	String firstName
+	String lastName
+	String email
+	String login
+	String password
+	Date birthDate
+	
+	static constraints = {
+		birthDate (nullable: true, max: new Date(Calendar.getInstance().getTime().year, Calendar.getInstance().getTime().month, Calendar.getInstance().getTime().date))
+		firstName blank:false, nullable:false, size:3..30, matches:"[a-zA-Z1-9_]+"
+		lastName blank:false, nullable:false, size:3..30, matches:"[a-zA-Z1-9_]+"
+		email email:true
+		login size: 5..15, blank: false, unique: true
+        password size: 5..15, blank: false
+	}
+
+	String toString(){
+		return firstName;
+	}
+}
