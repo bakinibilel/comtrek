15c08c555728badc25407787816eaa04fce1ee46
surt <surt@130.120.246.46>
2013-01-24 16:04:57 +0100

    spock user constraints test #10

--- comtrek/grails-app/domain/comtrek/User.groovy
+++ comtrek/grails-app/domain/comtrek/User.groovy
@@ -9,22 +9,26 @@ class User {
 
 	String firstName
 	String lastName
-	String bbb
+    String gender
 	String email
 	String login
 	String password
 	Date birthDate
 	
 	static constraints = {
-		birthDate (nullable: true, max: new Date(Calendar.getInstance().getTime().year, Calendar.getInstance().getTime().month, Calendar.getInstance().getTime().date))
+		birthDate (nullable: false, max: new Date(Calendar.getInstance().getTime().year, Calendar.getInstance().getTime().month, Calendar.getInstance().getTime().date))
 		firstName blank:false, nullable:false, size:3..30, matches:"[a-zA-Z1-9_]+"
 		lastName blank:false, nullable:false, size:3..30, matches:"[a-zA-Z1-9_]+"
+		gender inList: ["Male", "Female"]
 		email email:true
 		login size: 5..15, blank: false, unique: true
         password size: 5..15, blank: false
 	}
 
 	String toString(){
-		return firstName;
+		return "Login : "+login+
+				"\nName : "+ firstName +" "+lastName+ 
+				"\nbirth date : "+ birthDate+
+				"\nEmail : "+ email ;
 	}
 }
