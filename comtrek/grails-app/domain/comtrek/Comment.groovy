5798494fb11fc6185b4daf2468d0e8ac354128a3
bakinibilel <bakinibilel@gmail.com>
2013-01-31 11:54:58 +0100

    #19 correction class Comment

---
+++ comtrek/grails-app/domain/comtrek/Comment.groovy
@@ -0,0 +1,16 @@
+package comtrek
+
+class Comment {
+
+	int id
+	String comment
+	Date writing_date
+	
+    static constraints = {
+		
+		id blank: false, nullable: false, unique: true
+		comment blank:false, nullable:false, size:3..30, matches:"[a-zA-Z1-9_]+"
+		writing_date nullable:false
+		
+    }
+}
