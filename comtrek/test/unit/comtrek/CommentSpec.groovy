247b80c281262d3784c58615b57a3af1bb2b2566
bakinibilel <bakinibilel@gmail.com>
2013-02-10 13:48:38 +0100

    #22 Tests spock for Comment & Event

--- comtrek/test/unit/comtrek/CommentSpec.groovy
+++ comtrek/test/unit/comtrek/CommentSpec.groovy
@@ -1,7 +1,5 @@
 package comtrek
 
-
-
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 import spock.lang.Unroll
@@ -10,15 +8,11 @@ import grails.test.mixin.*
 import grails.test.mixin.support.*
 import org.junit.*
 
-/**
- * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
- */
-@TestFor(Event)
-class EventSpec extends Specification{
-
-    def setup() {
-		//mock an event with some data (put unique violations in here so they can be tested, the others aren't needed)
-		mockForConstraintsTests(Event, [new Event(averageNote:2 )])
+class CommentSpec extends Specification{
+	
+	def setup() {
+		//mock a comment with some data (put unique violations in here so they can be tested, the others aren't needed)
+		mockForConstraintsTests(Comment, [new Comment( )])
 	}
 	
 	void validateConstraints(obj, field, error) {
@@ -33,19 +27,20 @@ class EventSpec extends Specification{
 		//println ("erreur attendue : "+error+ ", pour le champ : "+field+ ", avec la valeur : "+obj."$field"+", erreur obtenu : "+obj.errors[field])
 	}
 	
-	@Unroll("test Event all constraints #field is #error using #val")
+	@Unroll("test Comment all constraints #field is #error using #val")
 	def "test Event all constraints"() {
 		when:
-		def obj = new Event("$field": val)
+		def obj = new Comment("$field": val)
 
 		then:
 		validateConstraints(obj, field, error)
 
 		where:
 		error                  | field                | val
-		"range"                |"NaverageNote"        | -1
-		"range"                |"AverageNote"         | 11
-		"nullable"             |"effectiveTime"       | null
+		"size"                 |"comment"             | 2
+		"size"                 |"comment"             | 31
+		"nullable"             |"writing_date"        | null
 
 	}
+
 }
